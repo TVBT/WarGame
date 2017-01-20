4b31bb86f1db079c8460af409512741c2d4f2581
Thinh Tran <ns.hermione@gmail.com>
2017-01-20 16:15:18 +0700

    init config

---
+++ client/app.js
@@ -0,0 +1,20 @@
+/**
+ * Created by thinhth2 on 1/20/2017.
+ */
+var SimpleGame = (function () {
+    function SimpleGame() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    }
+    SimpleGame.prototype.preload = function () {
+        this.game.load.image('logo', 'assets/images/blue.png');
+    };
+    SimpleGame.prototype.create = function () {
+        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+    };
+    return SimpleGame;
+}());
+window.onload = function () {
+    var game = new SimpleGame();
+};
+//# sourceMappingURL=app.js.map
