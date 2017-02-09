45aa4ef71f629b10bd87688fffc441dd68cc7b9c
binhben <lethanhbinh081@gmail.com>
2017-02-09 10:01:01 +0700

    now, tank can shoot

--- client/app/screens/play/play.screen.ts
+++ client/app/screens/play/play.screen.ts
@@ -20,40 +20,34 @@ export class PlayScreen implements AfterViewInit {
         });
 
         function preload() {
-
             game.load.tilemap('tilemap', 'assets/map/map.json?v=1', null, Phaser.Tilemap.TILED_JSON);
             game.load.image('map', 'assets/map/map.png');
             game.load.spritesheet('tank', 'assets/images/tank1.png', 32, 32);
-
+            game.load.image('bullet_up', 'assets/images/bullet_up.png');
         }
 
         var map;
         var layer;
         var cursors;
         var sprite;
+        var bullet;
+        var bullets;
 
         function create() {
             game.physics.startSystem(Phaser.Physics.ARCADE);
-
             //  Because we're loading CSV map data we have to specify the tile size here or we can't render it
             map = game.add.tilemap('tilemap', 16, 16, 80 * 16, 48 * 16);
-
             //  Now add in the tileset
             map.addTilesetImage('map');
-
             map.setCollisionBetween(1, 110);
-
             //  Create our layer
             layer = map.createLayer(0);
-
             //  Resize the world
             layer.resizeWorld();
-
 
             sprite = game.add.sprite(240, 100, 'tank');
             sprite.anchor.set(0.5);
             game.physics.enable(sprite);
-
             sprite.body.setSize(32, 32, 0, 0);
 
             cursors = game.input.keyboard.createCursorKeys();
@@ -62,6 +56,13 @@ export class PlayScreen implements AfterViewInit {
             sprite.animations.add("left", [1,4], 10, false);
             sprite.animations.add("right", [6,7], 10, false);
             sprite.animations.add("up", [2,5], 10, false);
+
+            bullets = game.add.group();
+            bullets.enableBody = true;
+            bullets.physicsBodyType = Phaser.Physics.ARCADE;
+            bullets.createMultiple(1, "bullet_up");
+            bullets.setAll("checkWorldBounds", true);
+            bullets.setAll("outOfBoundsKill", true);
         }
 
         function update() {
@@ -83,6 +84,41 @@ export class PlayScreen implements AfterViewInit {
             } else if (cursors.down.isDown) {
                 sprite.body.velocity.y = 100;
                 sprite.animations.play("down");
+            }
+
+            if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+                if (bullets.countDead() > 0) {
+                    bullet = bullets.getFirstDead();
+                    bullet.anchor.setTo(0.5, 0.5);
+                    bullet.reset(sprite.centerX, sprite.centerY);
+                    game.physics.enable(bullet);
+
+                    let aniName = sprite.animations.name;
+                    let speed = 300;
+                    switch (aniName) {
+                        case "left":
+                            bullet.angle = -90;
+                            bullet.body.velocity.x = -speed;
+                            break;
+                        case "right":
+                            bullet.angle = 90;
+                            bullet.body.velocity.x = speed;
+                            break;
+                        case "up":
+                            bullet.angle = 0;
+                            bullet.body.velocity.y = -speed;
+                            break;
+                        case "down":
+                            bullet.angle = -180;
+                            bullet.body.velocity.y = speed;
+                            break;
+                    }
+                }
+            }
+            if (bullet) {
+                game.physics.arcade.collide(bullet, layer, () => {
+                    bullet.kill();
+                });
             }
         }
 
